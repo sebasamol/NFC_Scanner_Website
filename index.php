515e7bf050d0e62d93b674d9fb6aae99870616c5
sebasamol <sebasamol@gmail.com>
2022-11-19 16:25:38 +0100

    Navbar

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang ="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width,initial-scale1.0">
+        <title>Wypożyczalnia strojów karnawałowych</title>
+    
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Raleway&family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/abd674511e.js" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><i class="fa-solid fa-masks-theater"></i>Wypożyczalnia strojów </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ms-auto">
+        <a class="nav-link px-lg-3 active" aria-current="page" href="#">Strona główna</a>
+        <a class="nav-link px-lg-3" href="#">Baza wypożyczeń</a>
+        <a class="nav-link px-lg-3" href="#">Klienci</a>
+        <a class="nav-link px-lg-3" href="#">Dodaj strój</a>
+      </div>
+    </div>
+  </div>
+</nav>
+      
+</body>
+
+
+    
+        
+
+   
+
+
+</html>
